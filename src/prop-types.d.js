0021ba33a7cb07f489b3a63d0161810958f8e238
SlowArmoredWarrior <slowarmoredwarrior99@gmail.com>
2024-01-11 12:54:15 +0000

    Added Props for some Components

--- src/prop-types.d.js
+++ src/prop-types.d.js
@@ -9,3 +9,12 @@ export const badgeProps = {
   state: PropTypes.number,
   text: PropTypes.string,
 };
+
+export const LayoutTypeProps = {
+  children: PropTypes.node,
+};
+
+export const AvatarPropTypes = {
+  state: PropTypes.number,
+  imgsrc: PropTypes.string,
+};
