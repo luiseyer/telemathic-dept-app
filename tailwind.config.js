4be69cb1cf2bd8242ed01b8ec204f9e07ac2b177
SlowArmoredWarrior <slowarmoredwarrior99@gmail.com>
2024-01-11 12:52:53 +0000

    Added Tailwind Paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './src/**/*.jsx',
+    './src/*.js',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
