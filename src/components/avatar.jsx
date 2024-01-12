32f2596b28b2aa80df9b216b5e820fd3ae5e0518
luiseyer <luisfernandorp2001@gmail.com>
2024-01-12 17:42:45 -0400

    Add badge according to Avatar component state

--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -1,14 +1,15 @@
 import { AvatarPropTypes } from '../prop-types.d';
 
 export default function Avatar({ imgsrc, state }) {
-  console.log(state);
   return (
-    <picture className="w-11 h-11">
+    <picture className="relative w-11 h-11 block">
       <img
         className="w-full h-full rounded-full border border-gray-300 p-[1px]"
         src={imgsrc}
         alt="user"
       />
+      {state === 1
+        && <span className="absolute bottom-0 right-0 block w-3 h-3 bg-green-500 rounded-full" />}
     </picture>
   );
 }
