637f155d94f35d34cac061a7700501fca133ecaf
SlowArmoredWarrior <slowarmoredwarrior99@gmail.com>
2024-01-12 16:47:58 +0000

    Fixing one function/ added jsdoc

--- src/utils/text-manage.js
+++ src/utils/text-manage.js
@@ -1,6 +1,31 @@
 // Emplace modifiers
-export const trimText = (text, limit) => text.substr(0, limit);
+
+/**
+ * Cuts the text if it exceeds the specified lenght, otherwise, returns the same text
+ * @param {string} text - the text to be cutted
+ * @param {number} limit - character limit
+ * @return {string}
+ */
+export const trimText = (text, limit) => {
+  if (text.length > limit) {
+    return text.substring(0, limit);
+  }
+
+  return text;
+};
 
 // Markdown modifiers
+
+/**
+ * Makes the text bold as specification of markdown docs
+ * @param {string} text - text to be bold
+ * @returns {string}
+ */
 export const inflateText = (text) => `*${text}*`;
+
+/**
+ * Makes the text italic as specification of markdown docs
+ * @param {string} text - text to be italic
+ * @returns {string}
+ */
 export const italicText = (text) => `_${text}_`;
