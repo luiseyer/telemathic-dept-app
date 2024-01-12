2129d0799e128f9c7e235aaac28bfd18b5c81160
luiseyer <luisfernandorp2001@gmail.com>
2024-01-12 17:41:19 -0400

    AppContainer width adjustment

--- src/layouts/app-container.jsx
+++ src/layouts/app-container.jsx
@@ -2,7 +2,7 @@ import { LayoutTypeProps } from '../prop-types.d';
 
 export default function AppContainer({ children }) {
   return (
-    <div className="w-screen h-screen flex flex-row bg-gray-50">
+    <div className="w-full h-screen flex flex-row bg-gray-50">
       {children}
     </div>
   );
