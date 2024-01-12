7f6291eb0098feeb57cd1a3f3b35d7683e1b45bb
SlowArmoredWarrior <slowarmoredwarrior99@gmail.com>
2024-01-12 16:59:51 +0000

    Added constans support

---
+++ src/utils/constants.js
@@ -0,0 +1,26 @@
+export const ICON_SIZE_SM = '1.2em';
+export const ICON_SIZE_MD = '1.5em';
+export const ICON_SIZE_LG = '1.8em';
+
+export const ACTIVITY_STATES = [
+  {
+    type: 'Available',
+    dotColor: '#0808f5',
+  },
+  {
+    type: 'Work in progress',
+    dotColor: '#0808f5',
+  },
+  {
+    type: 'Disconnected',
+    dotColor: '#0808f5',
+  },
+];
+
+export const ROLE_TYPES = [
+  'Admin',
+  'Employee',
+  'Watcher',
+];
+
+export const dummyIdleFunc = () => true;
